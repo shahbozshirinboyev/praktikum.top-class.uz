b38a30c6f8ae4e082caa9bfbfc7b7596562d970f
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-02-20 11:18:42 +0500

    Sign in page design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 import RootLayout from "./layouts/RootLayout";
 import ErrorPage from "./pages/Error/ErrorPage";
@@ -8,13 +10,23 @@ import SignUp from "./pages/Auth/SignUp";
 function App() {
   const routes = createBrowserRouter([
     {
-      path: "/",
-      element: <RootLayout />,
-      errorElement: <ErrorPage />
+      path: '/',
+      element: (
+        <ProtectedRoute>
+          <RootLayout />
+        </ProtectedRoute>
+      ),
+      errorElement: <ErrorPage />,
     },
+    { path: '/signin', element: <SignIn /> },
+    { path: '/signup', element: <SignUp /> },
   ]);
 
-  return <RouterProvider router={routes} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={routes} />
+    </AuthProvider>
+  );
 }
 
 export default App;
